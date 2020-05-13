c9687ae11be2420c186c056230a023c032bc75e6
x1hibi <kamisama@comunidad.unam.mx>
2020-05-13 18:58:41 -0500

    final barchart

--- d3BarChart/d3BarChart.js
+++ d3BarChart/d3BarChart.js
@@ -17,6 +17,7 @@ function makeRequest(type,url,callback,nameFile='data.json'){
             callback(jsonData)
         }else if(this.readyState==4 && this.status==0){
             debugLog("Status","network not avalible")
+            //this is called when the network is not avalible 
             fetch(nameFile).then(response=>{console.log(response) ; return response.json()} ).then(jsonResponse=>callback(jsonResponse))
         }else{
             debugLog("Status","the petition is not ready")
@@ -28,69 +29,53 @@ function makeRequest(type,url,callback,nameFile='data.json'){
 }   
 
 
-//Start the petition when the hmtl is load
+//Start the petition when the html is load
 window.addEventListener('load',makeRequest("GET",url,handleData))
-
 
 
 //this function handle the behabior of the page when get the data from the petition
 function handleData(data){
-    debugLog("handleData","enter to the funtion")
     let chartData=data.data
-    debugLog("data",chartData)
-    debugLog("step 1","start build a barchart!")
-
     let size=chartData.length
-    
-
     let height=500
     let width=1000
-
-    //we create a scale to the values bigger than the svg size
     let padding = 25
     let leftPadding= 60
+    //we declare and the variables to make a scale of y axis
+    let maxValueY=d3.max(chartData,d=>d[1])
+    const yScale=d3.scaleLinear().domain([0,maxValueY]).range([height-padding,padding])
+    const yAxis=d3.axisLeft(yScale)
+    //we declare and the variables to make a scale of x axis
+    let minValueX=d3.min(chartData,d=>d[0])
+    let maxValueX=d3.max(chartData,d=>d[0])
+    let timeScale = d3.scaleTime().domain([new Date(minValueX), new Date(maxValueX)]).range([0, width-50]);
+    let xAxis=d3.axisBottom(timeScale)
 
-        let minValueY=d3.min(chartData,d=>d[1])
-        let maxValueY=d3.max(chartData,d=>d[1])
-        const yScale=d3.scaleLinear().domain([0,maxValueY]).range([height-padding,padding])
-        const yAxis=d3.axisLeft(yScale)
-
-        let minValueX=d3.min(chartData,d=>d[0])
-        let maxValueX=d3.max(chartData,d=>d[0])
-    
-
-       const svg=d3.select("#title").append("svg").attr("width",width+25).attr("height",height)
-       let timeScale = d3.scaleTime()
-       .domain([new Date(minValueX), new Date(maxValueX)])
-         .range([0, width-50]);
-         var xAxis=d3.axisBottom(timeScale)
-
-
-    debugLog("scale",timeScale) 
-
-    //we add axes titles and title graf 
-    svg.append("text").attr("x",width/2).attr("y",padding).text("Gross Domestic Product Growing").attr("text-anchor","middle").attr("class","title")
+    //create a svg 
+    const svg=d3.select("#title").append("svg").attr("width",width+25).attr("height",height)
+    //we add axes, his labels and the title of them
+    svg.append("text").attr("x",width/2).attr("y",padding).text("Gross Domestic Product Growing US").attr("text-anchor","middle").attr("class","title")
     svg.append("text").attr("x",width/2).attr("y",height-padding/2).text("Date (Year)").attr("text-anchor","middle").attr("class","axisLabel")
     svg.append("text").attr("x",(-height/2)+padding).attr("y",15).text("Gross Domestic Product (Billion USD)").attr("text-anchor","middle").attr("transform","rotate(-90)").attr("class","axisLabel")
-
-
     svg.append("div").attr("id","tooltip")
     svg.append("g").attr("id","y-axis").attr("transform","translate("+leftPadding+","+(-padding+5)+")").call(yAxis)
     svg.append('g').attr("id","x-axis").attr("transform","translate("+(leftPadding)+","+(height-padding*2+5)+")").call(xAxis)
-    let bars=svg.selectAll("rect").data(chartData).enter().append("rect").attr("width",(width-50)/size).attr("height",d=>height-yScale(d[1])-padding).attr("x",(d,i)=>leftPadding+i*(width-50)/size).attr("y",d=>yScale(d[1])-padding+5).attr("class","bar").attr("fill",(d,i)=>"rgb(95,25,"+(255-i)+")").attr("data-date",d=>d[0]).attr("data-gdp",d=>d[1])
-    bars.on("mouseover",d=>{
+    
+    //We create a single rect for every value in the array data
+    let bars=svg.selectAll("rect") //we select all the rect created inside the svg
+                .data(chartData).enter() //we select all the data inside chardata
+                .append("rect") //create a rect for each value
+                .attr("width",(width-50)/size).attr("height",d=>height-yScale(d[1])-padding) //we define the width and the height of each bar 
+                .attr("x",(d,i)=>leftPadding+i*(width-50)/size).attr("y",d=>yScale(d[1])-padding+5) //we asign the position in x and y for them
+                .attr("class","bar").attr("fill",(d,i)=>"rgb(95,25,"+(255-i)+")") //we style these bars in order to make a gradient effect
+                .attr("data-date",d=>d[0]).attr("data-gdp",d=>d[1]) // we sert the data as atributes
+
+    bars.on("mouseover",d=>{// we declare actions for over mouse event and show the current data in a tooltip
         let tooltip=document.getElementById("tooltip")
         tooltip.style.opacity=1
-        tooltip.innerHTML=`Current data <br/>${d[1]} Billion USD <br/> ${d[0]}`;
+        tooltip.innerHTML=`<p><em>Current data</em> <br/><b style='display:inline'>${d[1]}</b> Billion USD <br/> <i>${d[0]}</i></p>`;
         tooltip.setAttribute("data-date",d[0])
-        tooltip.style.left=event.clientX+25
-        tooltip.style.top=event.clientY-75
-        debugLog("x",event.clientX);
-        debugLog("Y",event.clientY)
-    }).on("mouseout",d=>{
-        let tooltip=document.getElementById("tooltip")
-        tooltip.style.opacity=0
-    })
-    
-
+        tooltip.style.left=`${event.clientX+25}px`
+        tooltip.style.top=`${event.clientY-75}px`
+    }).on("mouseout",d=>{document.getElementById("tooltip").style.opacity=0}) //we hide the data when the mouse is not over the bar
 }
